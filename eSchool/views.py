9391e885f677a02db73fa5b59cafb35918fffb3f
efremOffice <143795390+efremOffice@users.noreply.github.com>
2024-06-14 21:33:30 +0300

    first commit

---
+++ eSchool/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+
+    return render(request,'index.html')
+
+def about(request):
+
+    return render(request,'courses.html')
+
+def course(request):
+
+    return render(request,'courses.html')
+
+def learing_path(request):
+
+    return render(request,'index.html')
+
+def eventes(request):
+
+    return render(request,'index.html')
+
+def priceing(request):
+
+    return render(request,'index.html')
+
+def contact(request):
+
+    return render(request,'index.html')
+
+def GetStart(request):
+
+    return render(request,'index.html')
